8765abd5e7e108add79e43cb16b4db8fda9592ae
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-05-13 03:29:06 +0900

    initial commit

---
+++ src/main/java/sopt/sopterm/sannumsan/SannumsanApplication.java
@@ -0,0 +1,13 @@
+package sopt.sopterm.sannumsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SannumsanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SannumsanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/sopterm/sannumsan/SannumsanApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.sopterm.sannumsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SannumsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
